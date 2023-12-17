ddd83ae7cedfb1884ae500a6719b2666f7efe629
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2023-12-17 13:01:19 +0530

    Delete Django_project directory

--- Django_project/ImageToMusic/music_generator/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MusicGeneratorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'music_generator'
--- Django_project/ImageToMusic/music_generator/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.upload_image, name='upload_image'),
-]
